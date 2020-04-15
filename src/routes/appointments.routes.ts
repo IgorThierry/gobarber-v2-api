62d4ef5c9cc89145db25a0d5566a1d435d589b1e
Igor Thierry <igorthierry15@gmail.com>
2020-04-15 09:23:38 -0300

    proj base

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,37 @@
+import { Router, Request, Response } from 'express';
+
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request: Request, response: Response) => {
+  const appointments = appointmentsRepository.all();
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request: Request, response: Response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointmentService = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointmentService.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+export default appointmentsRouter;
