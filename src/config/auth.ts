a8dde25c64152edc7879066a4dc746a378d2106b
Igor Thierry <igorthierry15@gmail.com>
2020-04-17 12:26:48 -0300

    routes auth

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '1c905bfbf4d5d56416b3bdd90d27187f',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,7 +1,8 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
-
 import { sign } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -31,9 +32,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, '1c905bfbf4d5d56416b3bdd90d27187f', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return { user, token };
