05d97b671448f94d12ab3afb49c18b69abb431be
Igor Thierry <igorthierry15@gmail.com>
2020-06-21 14:01:04 -0300

    Test updateUserAvatar

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import fs from 'fs';
 
 import IUsersRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
@@ -19,6 +20,9 @@ class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -29,16 +33,12 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      // deletar avatar anterior
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
