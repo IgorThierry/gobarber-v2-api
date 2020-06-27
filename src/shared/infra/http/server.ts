0fbb6b2bc5f4c2204340aca195249ea28eea2510
Igor Thierry <igorthierry15@gmail.com>
2020-06-27 18:12:15 -0300

    end of tests

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -15,11 +15,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use('/files', express.static(uploadConfig.tmpFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  //console.log(err);
+  console.log(err);
 
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
